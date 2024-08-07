f01a98a09b92a734dcaf82c8629609afa3bc784f
Vallabh2909 <vallabhwasule913@gmail.com>
2024-08-07 16:09:26 +0530

    Added animations to hero section

--- components/component/hero-section.jsx
+++ components/component/hero-section.jsx
@@ -1,23 +1,62 @@
+"use client";
 import Image from "next/image";
-import Link from "next/link";
 import { Button } from "../ui/button";
+import { motion } from "framer-motion";
+
+const textContainerVariants = {
+  hidden: { opacity: 1 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.05,
+    },
+  },
+};
+
+const letterVariants = {
+  hidden: { opacity: 0, y: 50 },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 const HeroSection = () => {
+  const title = "Hello, I'm Vallabh Wasule";
+  const description =
+    "I'm an aspiring backend engineer with a focus on DevOps and cloud solutions. I'm passionate about building scalable and reliable applications.";
+
   return (
     <section className="bg-gradient-to-b from-primary to-primary-foreground py-20 text-primary-foreground">
       <div className="container mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
           <div className="space-y-4">
-            <h1 className="text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl">
-              Hello, I&apos;m Vallabh Wasule
-            </h1>
-            <p className="text-lg">
-              Im an aspiring backend engineer with a focus on DevOps and cloud
-              solutions. I&apos;m passionate about building scalable and
-              reliable applications.
-            </p>
+            <motion.h1
+              className="text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl"
+              variants={textContainerVariants}
+              initial="hidden"
+              animate="visible"
+            >
+              {title.split("").map((char, index) => (
+                <motion.span key={index} variants={letterVariants}>
+                  {char}
+                </motion.span>
+              ))}
+            </motion.h1>
+            <motion.p
+              className="text-lg"
+              variants={textContainerVariants}
+              initial="hidden"
+              animate="visible"
+            >
+              {description.split("").map((char, index) => (
+                <motion.span key={index} variants={letterVariants}>
+                  {char}
+                </motion.span>
+              ))}
+            </motion.p>
             <Button className="w-full sm:w-auto mr-1">Explore Projects</Button>
-            <Button className="w-full sm:w-auto ">View Resume</Button>
+            <Button className="w-full sm:w-auto">View Resume</Button>
           </div>
           <div className="flex justify-center">
             <Image
